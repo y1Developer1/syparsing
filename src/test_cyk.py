33887e507268962fc2eecd4db40da394141b9180
y1Developer <y1.developer.1@gmail.com>
2023-03-28 21:54:14 +0900

    Create test_cyk.py

---
+++ src/test_cyk.py
@@ -0,0 +1,68 @@
+import unittest
+from syparsing.cyk import CYK
+from syparsing.rule import Rule
+
+class Test_test_1(unittest.TestCase):
+    def test_A(self):
+        left_term1 = "unittest"
+        right_terms1 = ["unit","test"]
+        left_term2 = "unit"
+        right_terms2 = ["UNIT"]
+        left_term3 = "test"
+        right_terms3 = ["TEST"]
+        rule1 = Rule(left_term1,right_terms1)
+        rule2 = Rule(left_term2,right_terms2)
+        rule3 = Rule(left_term3,right_terms3)
+        ruleset:set[Rule] = {rule1,rule2,rule3}
+        cyk = CYK(ruleset)
+        hasCNF = cyk.hasCNFGrammer()
+        self.assertEqual(hasCNF,True)
+
+    def test_B(self):
+        left_term1 = "unit"
+        right_terms1 = ["UNIT"]
+        left_term2 = "UNIT"
+        right_terms2 = ["FAIL UNIT"]
+        rule1 = Rule(left_term1,right_terms1)
+        rule2 = Rule(left_term2,right_terms2)
+        ruleset:set[Rule] = {rule1,rule2}
+        cyk = CYK(ruleset)
+        hasCNF = cyk.hasCNFGrammer()
+        self.assertEqual(hasCNF,False)
+    
+    def test_C(self):
+        left_term1 = "fail"
+        right_terms1 = ["fail unittest"]
+        left_term2 = "fail unittest"
+        right_terms2 = ["fail_unit","fail_test"]
+        rule1 = Rule(left_term1,right_terms1)
+        rule2 = Rule(left_term2,right_terms2)
+        ruleset:set[Rule] = {rule1,rule2}
+        cyk = CYK(ruleset)
+        hasCNF = cyk.hasCNFGrammer()
+        self.assertEqual(hasCNF,False)
+
+    def test_D(self):
+        left_term = "test"
+        right_terms = ["TEST"]
+        rule = Rule(left_term,right_terms)
+        ruleset:set[Rule] = {rule}
+        cyk = CYK(ruleset)
+        hasCNF = cyk.hasCNFGrammer()
+        self.assertEqual(hasCNF,True)
+
+    def test_E(self):
+        left_term = "unittest"
+        right_terms = ["unit","test"]
+        rule = Rule(left_term,right_terms)
+        ruleset:set[Rule] = {rule}
+        cyk = CYK(ruleset)
+        hasCNF = cyk.hasCNFGrammer()
+        self.assertEqual(hasCNF,False)
+
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
