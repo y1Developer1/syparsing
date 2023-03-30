24276aecfc1b657a2521f9e2b5fcb4293ad6b1e0
y1Developer <y1.developer.1@gmail.com>
2023-03-30 22:11:18 +0900

    edit rule

--- src/syparsing/rule.py
+++ src/syparsing/rule.py
@@ -16,29 +16,29 @@ class Rule:
     non_rule_input_msg = "non rule input"
     null_output_msg = "null output"
 
-    def __init__(self,left_term:str,right_terms:list[str]):
+    def __init__(self,left_term:str,right_terms:list[str])->None:
         self.setLeftTerm(left_term)
         self.setRightTerms(right_terms)
         
 
-    def getLeftTerm(self):
+    def getLeftTerm(self)->str:
         return self.l + ""
     
-    def getTheRightTerms(self):
+    def getTheRightTerms(self)->list[str]:
         copy_right_terms = []
         for term in self.r:
             copy_right_terms.append(term + "")
         return copy_right_terms
         
 
-    def setLeftTerm(self,new_left_term:str):
+    def setLeftTerm(self,new_left_term:str)->str:
         # check types of inputs
         if(not type(new_left_term) is str):
             raise ruleError(self.non_str_input_msg)
         # set the copies
         self.l = new_left_term + ""
 
-    def setRightTerms(self,new_right_terms:list[str]):
+    def setRightTerms(self,new_right_terms:list[str])->None:
         # check types of inputs
         if(not type(new_right_terms) is list):
             raise ruleError(self.non_list_input_msg)
@@ -52,7 +52,7 @@ class Rule:
         # set the copies
         self.r = copy_new_right_terms
     
-    def equals(self,another_rule):
+    def equals(self,another_rule)->bool:
         if(not type(another_rule) is Rule):
             raise ruleError(self.non_rule_input_msg)
         another_left_term = another_rule.getLeftTerm()
